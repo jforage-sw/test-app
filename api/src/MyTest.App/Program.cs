df8cc6e3cedea400c2014ebf45adda3fbb7b6a30
jforage-sw <james.forage@smartwyre.com>
2024-07-01 17:33:08 +0100

    test exclusions

--- api/src/MyTest.App/Program.cs
+++ api/src/MyTest.App/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// This class should be ignored by the SC analyzer
+
 using MyTest.App;
 
 Console.WriteLine(Helper.GetHelloWorld());
