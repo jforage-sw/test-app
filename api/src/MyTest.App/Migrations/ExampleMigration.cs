6c7438afdcb9fd4f18d14b825af8eb12c74c495b
jforage-sw <james.forage@smartwyre.com>
2024-07-02 15:00:53 +0100

    trigger SC

--- api/src/MyTest.App/Migrations/ExampleMigration.cs
+++ api/src/MyTest.App/Migrations/ExampleMigration.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyTest.App.Migrations;
 
-// This class should be ignored by the SC analyzer 
+// This class should be ignored by the SC analyzer
 public class ExampleMigration
 {
     public void Up()
