63effe2a8a656bac1b800823921d8e2c0653e89a
jforage-sw <james.forage@smartwyre.com>
2024-07-30 11:40:29 +0100

    add new method

--- api/src/MyTest.App/Helper.cs
+++ api/src/MyTest.App/Helper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
 
 namespace MyTest.App;
 
@@ -8,4 +9,12 @@ public static class Helper
     public static string GetHelloWorld() => "Hello, World!";
 
     public static string GetGoodbye() => "Goodbye!";
+
+    public static void DoStuff()
+    {
+        foreach (var i in Enumerable.Range(1, 5))
+        {
+            Console.WriteLine(i);
+        }
+    }
 }
